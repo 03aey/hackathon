75cc7c956bb1eff0e3f8d4e12fcdf741700f61ee
Alok Yadav <yalok6321@gmail.com>
2026-05-27 09:16:22 +0530

    add prisma and better auth configuration

---
+++ auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  database: prismaAdapter(prisma, { provider: 'postgresql' }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        defaultValue: 'PARTICIPANT',
+        input: false,
+      },
+    },
+  },
+});
